dcf2318926c8e1f3dfb8d82d813047c135fe5134
ihor-denysenko <denysenko.ihor2005@gmail.com>
2026-06-07 13:35:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -61,10 +61,9 @@ const map = L.map('map', {
 L.control.zoom({ position: 'bottomright' }).addTo(map);
 
 /* Подключение стилизованных слоев CartoDB Voyager */
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
-  subdomains: 'abcd',
-  maxZoom: 20
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  maxZoom: 17,
+  attribution: '&copy; OpenTopoMap'
 }).addTo(map);
 
 /* ── STATE ───────────────────────────────────────────────── */
